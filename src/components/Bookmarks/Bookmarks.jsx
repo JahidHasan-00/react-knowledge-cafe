e47385393229d296bc7679bddf969cbe36054507
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-04-16 22:00:00 +0600

    Finished all functionality with responsive

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -6,7 +6,7 @@ const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         
         <div className='w-2/5 p-7 bg-gray-300 ml-4'>
-            <div className='py-5 bg-[#6047EC1A] text-center border-2 rounded-lg border-[#6047EC1A]'>
+            <div className='py-5 bg-[#6047EC1A] text-center border-2 rounded-lg text-blue-600 border-[#6047EC1A]'>
                 <h3 className="font-bold text-2xl"> Reading Time: {readingTime} </h3>
             </div>
             <h2 className='font-bold text-2xl text-center'>Bookmarked Blogs: {bookmarks.length}</h2>
